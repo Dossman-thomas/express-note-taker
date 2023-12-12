17c173d1635ec27f6efb3dd7912dfcee9ba53d77
Tom Dossman <tdossman.development@gmail.com>
2023-12-12 13:45:02 -0500

    modularized files and set up express server

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+// Import express
+const express = require('express');
+
+// Setting the PORT
+const PORT = process.env.PORT || 3001; 
+
+// Create an instance of express app
+const app = express();
+
+// Parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // parse incoming requests w/URL-encoded payloads to handle form data. 
+// Configuring middleware
+app.use(express.static("public")); // Serves static files from "public" directory
+
+// Using modularized html and Api routes
+app.use(htmlRoutes);
+app.use(apiRoutes); 
+
+// Starting the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`); // cb function executed once the server successfully starts.
+});
